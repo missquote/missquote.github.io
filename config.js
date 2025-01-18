ea6f5a959999731e9a77fcc6e2d2076fb4bb9599
missquote <ethacker20@gmail.com>
2025-01-18 11:00:02 -0500

    site setup

--- config.js
+++ config.js
@@ -1,80 +1,41 @@
 // @ts-check
 import profileImage from './src/images/profilepic.png?url';
-import backgroundImage from './src/images/backgrounds/watercolor-blue-green.png?url';
-import productImage from './src/images/product1.png?url';
+import backgroundImage from './src/images/backgrounds/watercolor-pink-blue.png?url';
 
 /** @type {import('./src/types').SiteConfig} */
 const config = {
   user: {
-    name: "Cory Trimm",
-    bio: "Serial builder of things - software, hardware, and everything in between. This is my little link in bio project built with Astro and Tailwind.",
+    name: "emily ✨",
+    bio: "just another internet nerd",
     profileImage: profileImage,
   },
   background: {
     color: "#f0f0f0",
     image: backgroundImage,
   },
-  socialLinks: [
-    {
-      platform: "instagram",
-      url: "https://instagram.com/journey.unknown",
-      icon: "camera-retro",
-    },
-    {
-      platform: "threads",
-      url: "https://threads.net/@journey.unknown",
-      icon: "threads",
-    },
-    {
-      platform: "linkedin",
-      url: "https://linkedin.com/in/cory-trimm",
-      icon: "linkedin",
-    },
-    {
-      platform: "spotify",
-      url: "https://spotify.com",
-      icon: "spotify",
-    },
-    {
-      platform: "snapchat",
-      url: "https://snapchat.com",
-      icon: "snapchaat",
-    },
-    {
-      platform: "tiktok",
-      url: "https://tiktok.com",
-      icon: "tiktok",
-    },
-    // Add more social links as needed
-    // Search for icons here - https://fontawesome.com/search
-  ],
+
+  socialLinks: [],
+
   links: [
     {
-      title: "Use This Template",
-      url: "https://github.com/ctrimm/astro-link-in-bio-theme",
-      icon: "code",
+      title: "my blog",
+      url: "https://blog.missquote.net",
+      icon: "blog",
     },
     {
-      title: "My Personal Web Site",
-      url: "https://corytrimm.com",
+      title: "follow me on the fediverse",
+      url: "https://sakurajima.social/@missquote",
       icon: "globe",
     },
     {
-      title: "Join My YouTube Channel",
-      url: "https://youtube.com/c/corytrimm",
-      icon: "play",
-    },
+      title: "picrew by ummmmandy",
+      url: "https://picrew.me/en/image_maker/114808",
+      icon: "image-portrait"
+    }
     // Add more links as needed
   ],
   products: [
-    {
-      title: "Digital Product",
-      description: "Description of your digital product",
-      url: "https://inshortpod.com",
-      price: 19.99,
-      includePriceOnSite: true,
-      image: productImage,
-    },
+
     // Add more products as needed
   ],
 };
